3d7c7d5d44ed7c56ae4afeba7361ce88a79a06a4
Charles <charlesangeloplai@gmail.com>
2020-11-29 23:32:59 -0500

    commit tree d65ffabda0ab93fdf33fa1c1449d4889ce7eab06

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,22 @@
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'pry'
+
 # Write your code here
+class GetRequester
+    attr_reader :uri
+
+    def initialize(url)
+        @uri = URI.parse(url)
+    end
+
+    def get_response_body
+        response = Net::HTTP.get_response(uri)
+        response.body
+    end
+
+    def parse_json
+        JSON.parse(get_response_body)
+    end
+end
